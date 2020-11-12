a59c0067d9f7881d7beb77b0fc672a7b7ee0a21f
VM <johannes@schmid-company.de>
2020-11-12 16:55:32 +0100

    changes

--- jumpcut.py
+++ jumpcut.py
@@ -8,12 +8,12 @@ if __name__ == '__main__':
         description='Modifies a video file to cut out silence.')
     parser.add_argument('-i', '--input_file', type=str,
                         help='the video file you want modified')
-    parser.add_argument('--output_file', type=str,  help="the output file")
-    parser.add_argument('--dcb_threshold', type=int, default=10,
+    parser.add_argument('-o', '--output_file', type=str,  help="the output file")
+    parser.add_argument('-t', '--dcb_threshold', type=int, default=10,
                         help="the threshold accepted as \"silence\" in dcb")
-    parser.add_argument('--keep_silence', type=float, default=0.2,
+    parser.add_argument('-k', '--keep_silence', type=float, default=0.2,
                         help="amount of distance from silence to audio in s")
-    parser.add_argument('--silent_length', type=int, default=500,
+    parser.add_argument('-l', '--silent_length', type=int, default=500,
                         help="the miminum amount of silence in ms")
 
     args = parser.parse_args()
@@ -23,6 +23,7 @@ if __name__ == '__main__':
     DCB_THRESHOLD = args.dcb_threshold
     KEEP_SILENCE = args.keep_silence
     SILENT_LENGTH = args.silent_length
+    PROCESS_COUNT = 4
 
     assert INPUT_FILE is not None, "why u put no input file, that dum"
     if OUTPUT_FILE is None:
@@ -42,5 +43,12 @@ if __name__ == '__main__':
 
     print(length)
     # -------------------------------------------------- MULTI
+    seconds = math.ceil(length / PROCESS_COUNT)
+
+    command = 'ffmpeg -i ' + args.input_file + \
+        ' -c copy -map 0 -segment_time ' + seconds + \
+        ' -f segment chunk%%03d.mp4'
+    subprocess.call(command, shell=True)
+
 
     # -------------------------------------------------- COMBINE
