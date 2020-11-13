3c698af1373d5b31e1b3d58d4b070b157f1acb6b
StefanHamm <stefan.j.hamm@gmail.com>
2020-11-13 01:09:38 +0100

    get_video_length

--- silencer.py
+++ silencer.py
@@ -1,8 +1,17 @@
 from pydub import silence
 from pydub import AudioSegment
-
+from moviepy.editor import *
 
 def silencer(audio_wav, dcb_offset=10, silent_length=500,step_in_ms=10):
     Pure_audio = AudioSegment.from_wav(audio_wav)
     dbc = Pure_audio.dBFS
     return silence.detect_silence(audio_segment=Pure_audio, silence_thresh=dbc - dcb_offset, min_silence_len=silent_length, seek_step=step_in_ms)
+
+
+
+def get_video_length(file_name):
+    clip = VideoFileClip(file_name)
+    duration = clip.duration
+    clip.close
+    return duration
+     
