7cae595fd85df0063cff34b8d860e7f86e9b3030
StefanHamm <stefan.j.hamm@gmail.com>
2020-11-14 03:42:50 +0100

    put everything into a class called Video new input folder for multiple videos removed Temp argument now integrated in class auto cleanup of temp folder

--- jumpcut.py
+++ jumpcut.py
@@ -6,112 +6,117 @@ import shutil
 
 from silencer import *
 
+
 def deletePath(s):
-    try:  
-        shutil.rmtree(s,ignore_errors=False)
-    except OSError:  
-        print ("Deletion of the directory %s failed" % s)
+    try:
+        shutil.rmtree(s, ignore_errors=False)
+    except OSError:
+        print(f"Deletion of the directory {s} failed")
         print(OSError)
+
 
 def createPath(s):
     if os.path.exists(s):
         deletePath(s)
-    try:  
+    try:
         os.mkdir(s)
-    except OSError:  
-        assert False, "Creation of the directory %s failed. (The TEMP folder may already exist. Delete or rename it, and try again.)"
+    except OSError:
+        assert False, f"Creation of the directory {s} failed. (The TEMP folder may already exist. Delete or rename it, and try again.)"
 
+
+def output_name(name):
+    dotIndex = name.rfind(".")
+    return name[:dotIndex] + "_ALTERED" + name[dotIndex:]
+
+
+class Video:
+    def __init__(self, input_file, output_path="", input_path="", output_file=None):
+        self.input_file = input_file
+        if output_file:
+            self.output_file = output_file
+        else:
+            self.output_file = input_file[0:-4] + "_sil.mp4"
+        self.input_path = input_path
+        self.output_path = output_path
+        self.temp_folder = "TEMP_" + input_file[:-4] + "/"
+        createPath("TEMP_" + input_file[:-4])
+        self.video_length = get_video_length(input_path + input_file)
+        self.arr_silence_ms = []
+        self.arr_audio_s = []
+
+    def __del__(self):
+        shutil.rmtree(self.temp_folder)
+        # deleting Temp folder
+
+    def extract_audio(self):
+        ffmpeg_get_audio(self.input_path + self.input_file, self.temp_folder + "audio.wav")
+
+    def detect_silence(self):
+        self.arr_silence_ms = silencer(self.temp_folder + "audio.wav", DCB_THRESHOLD, SILENT_LENGTH, SEEK_STEP)
+        self.arr_silence_ms.append((self.video_length, self.video_length))
+
+    def create_arr_audio_s(self):
+        last = 0.0
+        for x, y in self.arr_silence_ms:
+            start = last
+            end = x / 1000
+            if end - start > 0.01:
+                start = max(start - KEEP_SILENCE, 0)
+                end = min(end + KEEP_SILENCE, self.video_length)
+                self.arr_audio_s.append((round(start, 3), round(end, 3)))
+            last = y / 1000
+
+    def cut_array(self):
+        ffmpeg_cut_array(file_input=self.input_path + self.input_file,
+                         file_output=self.output_path + self.output_file
+                         , temp_file=self.temp_folder + "script.txt", timearray=self.arr_audio_s)
+
+    def work(self):
+        self.extract_audio()
+        self.detect_silence()
+        self.create_arr_audio_s()
+        self.cut_array()
 
 
 # -------------------------------------------------- ARGS
 
 parser = argparse.ArgumentParser(description="Modifies a video file to cut out silence.")
-parser.add_argument("-i", "--input_file",       type = str,                      help = "the video file you want modified")
-parser.add_argument("-o", "--output_file",      type = str,                      help = "the output file")
-parser.add_argument("-t", "--temp_path",        type = str,     default="TEMP/", help = "the temp directory")
-parser.add_argument("-d", "--dcb_threshold",    type = int,     default=10,      help = "the threshold accepted as \"silence\" in dcb")
-parser.add_argument("-k", "--keep_silence",     type = float,   default=0.2,     help = "amount of distance from silence to audio in s")
-parser.add_argument("-l", "--silent_length",    type = int,     default=500,     help = "the miminum amount of silence in ms")
-parser.add_argument("-s", "--seek_step",        type = int,     default=10,      help = "the audio step size in ms")
+parser.add_argument("-i", "--input_file", type=str, help="the video file you want modified")
+parser.add_argument("-o", "--output_file", type=str, help="the output file")
+parser.add_argument("-ip", "--input_path", type=str, default="INPUT/", help="the input directory")
+parser.add_argument("-op", "--output_path", type=str, default="OUTPUT/", help="the Output directory")
+parser.add_argument("-d", "--dcb_threshold", type=int, default=10, help="the threshold accepted as \"silence\" in dcb")
+parser.add_argument("-k", "--keep_silence", type=float, default=0.2,
+                    help="amount of distance from silence to audio in s")
+parser.add_argument("-l", "--silent_length", type=int, default=500, help="the miminum amount of silence in ms")
+parser.add_argument("-s", "--seek_step", type=int, default=10, help="the audio step size in ms")
 
 args = parser.parse_args()
-
-TEMP_PATH =  args.temp_path
+INPUT_DIR = args.input_path
+OUTPUT_DIR = args.output_path
 INPUT_FILE = args.input_file
 OUTPUT_FILE = args.output_file
 DCB_THRESHOLD = args.dcb_threshold
 KEEP_SILENCE = args.keep_silence
 SILENT_LENGTH = args.silent_length
 SEEK_STEP = args.seek_step
+INPUT_FILES_NAMES = os.listdir(INPUT_DIR)
+OUTPUT_FILES_NAMES = []
 
-assert INPUT_FILE is not None, "why u put no input file, that dum"
-if OUTPUT_FILE is None:
-    dotIndex = INPUT_FILE.rfind(".")
-    OUTPUT_FILE = INPUT_FILE[:dotIndex]+"_ALTERED"+INPUT_FILE[dotIndex:]
+assert (INPUT_FILE is not None) or (len(INPUT_FILES_NAMES) > 0), "why u put no input file-s, that dum"
 
 t = time.time()
-# -------------------------------------------------- CREATE TEMP FOLDER
+# --------------------------------------- creates arr of Vid class
+video_arr = []
+if INPUT_FILE is not None:
+    video_arr.append(Video(input_file=INPUT_FILE, output_file=OUTPUT_FILE))
 
-createPath(TEMP_PATH)
-video_length = get_video_length(INPUT_FILE)
-print(f"    cutting Video of length {video_length}")
+elif len(INPUT_FILES_NAMES) > 0:
+    for input_filename in INPUT_FILES_NAMES:
+        video_arr.append(Video(input_file=input_filename, output_path=OUTPUT_DIR, input_path=INPUT_DIR))
 
-# -------------------------------------------------- EXTRACT AUDIO
+for vid in video_arr:
+    vid.work()
 
-ffmpeg_get_audio(INPUT_FILE, TEMP_PATH + "audio.wav")
-
-# -------------------------------------------------- FIND SILENCE
-
-arr_silence_ms = silencer(TEMP_PATH + "audio.wav", DCB_THRESHOLD, SILENT_LENGTH, SEEK_STEP)
-print(f"\n    detectet {len(arr_silence_ms)} silences\n")
-
-# -------------------------------------------------- CREATE AUDIO ARRAY
-
-arr_silence_ms.append((video_length, video_length))
-arr_audio_s = []
-last = 0.0
-for x, y in arr_silence_ms:
-    start = last
-    end = x/1000
-    if end-start > 0.01:
-        start = max(start-KEEP_SILENCE, 0)
-        end = min(end+KEEP_SILENCE, video_length)
-        arr_audio_s.append((round(start, 3), round(end, 3)))
-    last = y/1000
-
-# for i in range(len(arr_audio_s)):
-#     print(str(arr_silence_ms[i]) + " -> " + str(arr_audio_s[i]))
-
-print(f"\n    created  {len(arr_audio_s)} parts\n")
-
-# -------------------------------------------------- CUT CHUNKS FROM ORIGINAL
-
-print(f"\n    cutting...\n")
-
-ffmpeg_cut_array(INPUT_FILE, OUTPUT_FILE, TEMP_PATH + "script.txt",  arr_audio_s)
-
-# i = 0
-# for start, end in arr_audio_s:
-#     print(f"\n    task {i} cutting...\n")
-
-#     name = "chunk" + str(i) + ".mp4"
-#     ffmpeg_cut_from_original(INPUT_FILE, TEMP_PATH + name, start, end)
-#     "file '" + name + "'\n"
-
-#     i+=1
-    
-
-# -------------------------------------------------- COMBINE CHUNKS
-# time.sleep(1)
-# print(f"\n    combining...\n")
-# ffmpeg_combile(TEMP_PATH + "list.txt", OUTPUT_FILE)
-
-# -------------------------------------------------- DELETE TEMP FOLDER
-
-print(f"\n    deleting temp files...\n")
-#! deletePath(TEMP_PATH)
-
-# -------------------------------------------------- FINISHED
-
-Tnow = time.time()-t
+Tnow = time.time() - t
 print("\nfinished in %f seconds" % (Tnow))
