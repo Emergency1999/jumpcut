92ba0254dcc404459d3e3201268443a4dbcf7a61
Johannes <johannes@schmid-company.de>
2020-11-20 11:57:10 +0100

    removed bugfix if length = 1

--- ownffmpeg.py
+++ ownffmpeg.py
@@ -13,23 +13,27 @@ def ffmpeg_cut_array(file_input, file_output, temp_file, timearray):
     i = 0
     for start, end in timearray:
         full_length += end - start
-        f.write(f"[0:v]trim=start={str(start)}:end={str(end)},setpts=PTS-STARTPTS[vpart{i}];")
-        f.write(f"[0:a]atrim=start={str(start)}:end={str(end)},asetpts=PTS-STARTPTS[apart{i}];")
+        f.write(f"[0:v]trim=start={str(start)}:end={str(end)},setpts=PTS-STARTPTS[v{i}];")
+        f.write(f"[0:a]atrim=start={str(start)}:end={str(end)},asetpts=PTS-STARTPTS[a{i}];")
         i+=1
     length = i
     i = 0
-    for i in range(length):
-        f.write(f"[vpart{i}][apart{i}]")
-    f.write(f"concat=n={length}:v=1:a=1[vout][aout]")
+    if length > 1:
+        for i in range(length):
+            f.write(f"[v{i}][a{i}]")
+        f.write(f"concat=n={length}:v=1:a=1[v{length}][a{length}]")
+        last = length
+    else:
+        last = i
     f.close()
 
     # execute script
-    command = f"ffmpeg -y -hide_banner -loglevel warning -i \"{file_input}\" -filter_complex_script \"{temp_file}\" -safe 0 -map [vout] -map [aout] -to {full_length} \"{file_output}\""
+    command = f"ffmpeg -y -hide_banner -loglevel warning -i \"{file_input}\" -filter_complex_script \"{temp_file}\" -async 1 -safe 0 -map [v{last}] -map [a{last}] -to {full_length} \"{file_output}\""
     # print(command)
     subprocess.call(command, shell=True)
 
-def ffmpeg_cut_from_original(file_input, file_output, start, end):
-    command = f"ffmpeg -y -hide_banner -loglevel warning -i \"{file_input}\" -ss {str(start)} -to {str(end)} \"{file_output}\""
+def ffmpeg_cut(file_input, file_output, start, end, additional_flag=""):
+    command = f"ffmpeg -y -hide_banner -loglevel warning -i \"{file_input}\" -ss {str(start)} -to {str(end)} {additional_flag} \"{file_output}\""
     subprocess.call(command, shell=True)
 
 def ffmpeg_combine(file_input, file_output):
